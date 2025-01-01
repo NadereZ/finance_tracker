635ac0b853df5540d7a35cf9bbeb928c8b62a729
nadere <naderezaboli@gmail.com>
2025-01-01 16:34:27 +0330

    Build models.

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+class Transaction(models.Model):
+    INCOME = 'income'
+    EXPENSE = 'expense'
+    TRANSACTION_TYPE_CHOICES = [
+        (INCOME, 'Income'),
+        (EXPENSE, 'Expense')
+    ]
+
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, related_name='transactions', on_delete=models.CASCADE)
+    description = models.TextField(null=True, blank=True)
+    transaction_type = models.CharField(max_length=10, choices=TRANSACTION_TYPE_CHOICES, default=EXPENSE)
+
+    def __str__(self):
+        return f"{self.transaction_type.title()} - {self.amount}"
+    
+class Budget(models.Model):
+    category = models.ForeignKey(Category, related_name='budgets', on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.category.name} Budget: {self.amount} ({self.start_date} to {self.end_date})"
+    
